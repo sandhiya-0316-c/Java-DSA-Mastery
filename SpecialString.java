2afe4aa389ea3596544ba31514faf689d006269e
sandhiya D <sandhiyad@sandhiyas-Air-2.mshome.net>
2026-02-20 11:53:22 +0530

    day2 - hard 1

---
+++ SpecialString.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class SpecialString {
+
+    public static String makeLargestSpecial(String s) {
+
+        if (s.length() <= 2) return s;
+
+        List<String> list = new ArrayList<>();
+        int count = 0;
+        int start = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '1') count++;
+            else count--;
+
+            if (count == 0) {
+                String inner = s.substring(start + 1, i);
+                list.add("1" + makeLargestSpecial(inner) + "0");
+                start = i + 1;
+            }
+        }
+
+        Collections.sort(list, Collections.reverseOrder());
+
+        StringBuilder result = new StringBuilder();
+        for (String str : list) {
+            result.append(str);
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String input = "11011000";
+        System.out.println(makeLargestSpecial(input));
+    }
+}
